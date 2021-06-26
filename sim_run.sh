aa03ad9059b408e69ccd87e993f4009324b98432
alexie <anpogue@ucla.edu>
2021-06-25 19:45:42 -0700

    few edits

--- sim_run.sh
+++ sim_run.sh
@@ -5,12 +5,16 @@ REPEAT_VAR="2"
 #./build/sim_vis_fixed/sim_opt_fixed $REPEAT_VAR
 #./build/sim_vis_fixed/sim_em_fixed $REPEAT_VAR
 #./build/sim_vis_fixed/sim_boem_fixed $REPEAT_VAR
+#python sim_eval.py
+#python eval_sim_fig2.py $REPEAT_VAR
+
 
 #
 #./build/sim_vis_win/sim_opt_win $REPEAT_VAR
 #./build/sim_vis_win/sim_em_win $REPEAT_VAR
 #./build/sim_vis_win/sim_boem_win $REPEAT_VAR
 
+# this value will be the loop value k for trial window length
 
 REPEAT_VAR="2"
 
@@ -18,6 +22,5 @@ perf stat -r $REPEAT_VAR ./build/sim_vis_win/sim_opt_win $REPEAT_VAR
 
 
 
-#python sim_eval.py
-#python eval_sim_fig2.py $REPEAT_VAR
+
 
